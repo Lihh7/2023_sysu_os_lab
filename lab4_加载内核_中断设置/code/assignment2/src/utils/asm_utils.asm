cb3bb6c1960c6b31228b5cb6bc55c852da9c54b0
kuuuure <127190339+kuuuure@users.noreply.github.com>
2023-07-08 21:10:24 +0800

    Add files via upload

---
+++ lab4_加载内核_中断设置/code/assignment2/src/utils/asm_utils.asm
@@ -0,0 +1,44 @@
+[bits 32]
+
+global asm_hello_world
+
+asm_hello_world:
+    push eax
+    xor eax, eax
+
+    mov ah, 0x03 ;青色
+    mov al, '2'
+    mov [gs:2 * 0], ax
+
+    mov al, '1'
+    mov [gs:2 * 1], ax
+
+    mov al, '3'
+    mov [gs:2 * 2], ax
+
+    mov al, '0'
+    mov [gs:2 * 3], ax
+
+    mov al, '7'
+    mov [gs:2 * 4], ax
+
+    mov al, '0'
+    mov [gs:2 * 5], ax
+
+    mov al, '1'
+    mov [gs:2 * 6], ax
+
+    mov al, '8'
+    mov [gs:2 * 7], ax
+
+    mov al, 'l'
+    mov [gs:2 * 8], ax
+
+    mov al, 'h'
+    mov [gs:2 * 9], ax
+
+    mov al, 'h'
+    mov [gs:2 * 10], ax
+
+    pop eax
+    ret
---
+++ lab4_加载内核_中断设置/code/assignment3/include/asm_utils.h
@@ -0,0 +1,12 @@
+#ifndef ASM_UTILS_H
+#define ASM_UTILS_H
+
+#include "os_type.h"
+
+extern "C" void asm_hello_world();
+extern "C" void asm_lidt(uint32 start, uint16 limit);
+extern "C" void asm_unhandled_interrupt();
+extern "C" void asm_halt();
+extern "C" void asm_my_interrupt();
+
+#endif
